e8af12c03af5bf4a2c277110d691764a8a55d5a7
manoelfilho <manoel.tsf@gmail.com>
2021-02-15 15:22:20 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Manoel Filho on 15/02/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
